b220431eba03055541c5d65b652b778e31b9818e
Noctiz <noctizgame@gmail.com>
2017-05-22 20:03:07 -0700

    geometry unit test

--- Assets/Scripts/GameManagement/NOCTerrainBuilder.cs
+++ Assets/Scripts/GameManagement/NOCTerrainBuilder.cs
@@ -123,6 +123,7 @@ public class NOCTerrainBuilder : MonoBehaviour {
 		}
 	}
 
+	// Unit Tests //
 	private void TestGeometryDemoBuild ()
 	{
 		NOCGeometryTest geo = ReadGeometryFromFile("NOCJSONDemo");
