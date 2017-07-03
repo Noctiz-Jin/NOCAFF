66f6fe3cae4871e0b9a596a7df1b051e519e32b4
Noctiz <noctizgame@gmail.com>
2017-07-03 16:42:54 -0700

    handle object and sword init

--- Assets/Scripts/Players/NOCPlayerHands.cs
+++ Assets/Scripts/Players/NOCPlayerHands.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class NOCPlayerHands : MonoBehaviour {
 
 	private NOCPlayerAnimator playerAnimator;
+	private static string leftHandLookUp = "Armature/Root/Hip/Spine/Upper_Arm_L/Lower_Arm_L/Hand_L/LeftHandHold";
+	private static string rightHandLookUp = "Armature/Root/Hip/Spine/Upper_Arm_R/Lower_Arm_R/Hand_R/RightHandHold";
+	private Transform leftHandHold;
+	private Transform rightHandHold;
 
 	void Start () {
 		playerAnimator = GetComponent<NOCPlayerAnimator>();
+		leftHandHold = gameObject.transform.Find(leftHandLookUp);
+		rightHandHold = gameObject.transform.Find(rightHandLookUp);
 	}
 	
 	public void GetHandsInputAndAct()
@@ -15,11 +21,24 @@ public class NOCPlayerHands : MonoBehaviour {
 		if (Input.GetButtonDown("LeftHandAct"))
 		{
 			playerAnimator.PlayerHandHold(true);
+			GrabWithLeftHand(true);
 		}
 
 		if (Input.GetButtonDown("RightHandAct"))
 		{
 			playerAnimator.PlayerHandHold(false);
+			GrabWithLeftHand(false);
+		}
+	}
+
+	private void GrabWithLeftHand(bool isLeft)
+	{
+		GameObject sword = GameObject.Find("NOCSwordHandle");
+		if (isLeft)
+		{
+			sword.GetComponent<NOCHandle>().GrabHandle(leftHandHold, leftHandHold);
+		} else {
+			sword.GetComponent<NOCHandle>().GrabHandle(rightHandHold, rightHandHold);
 		}
 	}
 }
---
+++ Assets/Scripts/Utility/NOCHandle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NOCHandle : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	public void GrabHandle(Transform transform, Transform parent)
+	{
+		gameObject.transform.position = transform.position;
+		gameObject.transform.rotation = transform.rotation;
+		gameObject.transform.SetParent(parent);
+	}
+}
